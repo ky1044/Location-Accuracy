83208766419010ae4986f2a6e20ac0151f8f73f2
Ken Yokokawa <ky1044@nyu.edu>
2019-07-16 02:30:53 -0400

    removing unnneeded lines from code

--- Location Accuracy in NY.R
+++ Location Accuracy in NY.R
@@ -18,23 +18,12 @@ nyloc$lon = nyloc$longitudeE7/ 1e7
 #log accuracy so that high outliers don't mess up color scale
 nyloc$logAccuracy = log(nyloc$accuracy,base=exp(2))
 
-hist(as.numeric(loc$timestampMs))
 
 ggmap(map) +
   geom_point(data=nyloc,aes(x = lon, y = lat, color = logAccuracy)) + scale_colour_gradient(low = "white", high = "blue")+
   labs( x = "Longitude", y = "Latitude", title = "Location Accuracy (lower value is more accurate)")
 
-#ggsave("My Location Accuracy in Midtown.jpeg")
-
-
-nyloc$distance <- function(i) {
-  mapdist(as.numeric(a[i, c('nyloc.Lon','nyloc.lat')]), 
-          as.numeric(a[i, c('-73.99','40.73')]), 
-          mode='driving')$km
-}
-
-nyloc$dist_km <- mapdist(from = paste0(as.character(nyloc$lat),",",as.character(nyloc$lon)),
-                     to = paste0(as.character('-73.99'),", ",as.character('40.73')))[,c('km')]
+ggsave("My Location Accuracy.jpeg")
 
 
 
